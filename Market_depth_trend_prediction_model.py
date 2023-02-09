76073474cc364b34a20b74c5a07ad036b5ccd77c
spawnaga <spawnaga@gmail.com>
2023-02-08 22:01:53 -0700

    update files

--- Market_depth_trend_prediction_model.py
+++ Market_depth_trend_prediction_model.py
@@ -1,40 +1,102 @@
 import datetime
 import sqlite3
 import pandas as pd
+import numpy as np
 import tensorflow as tf
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import MinMaxScaler
+from sklearn.preprocessing import MinMaxScaler, LabelEncoder
+from sklearn.metrics import accuracy_score, log_loss
+from sklearn.linear_model import LinearRegression, LogisticRegression, Perceptron
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.neural_network import MLPClassifier
+from sklearn.svm import SVC
+from sklearn import tree
+from xgboost import XGBClassifier
 from tensorflow.keras.callbacks import EarlyStopping
-from tensorflow.keras.layers import Input, Conv1D, Flatten, Dense, Dropout, MaxPool1D
+from tensorflow.keras.layers import Input, Conv1D, Flatten, Dense, Dropout, MaxPool1D, LSTM, RNN, SimpleRNN, Reshape
 from tensorflow.keras.models import Model
 from tensorflow.keras.utils import to_categorical
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.callbacks import LearningRateScheduler
+import math
 
 
 class ModelTraining:
     def __init__(self, database_path):
-
+        self.X = None
+        self.y = None
+        self.X_train = None
+        self.X_test = None
+        self.y_test = None
+        self.y_train = None
+        self.model = None
         self.db = sqlite3.connect(database_path)
         self.df = pd.read_sql_query("SELECT * from ES_market_depth", self.db)
-        self.early_stop = EarlyStopping(monitor='val_loss', mode='min', verbose=1, patience=50)
+        self.early_stop = EarlyStopping(monitor='val_loss', mode='min', verbose=1, patience=20)
         self.scaler = MinMaxScaler()
         self.logdir = "logs/fit/" + "agentLearning" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
         self.tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=self.logdir)
+        self.optimizer = Adam(learning_rate=0.01)
+        self.lr_schedule = LearningRateScheduler(lambda epoch: 0.001 * (0.1 ** (epoch // 10)))
+        self.models = {}
 
     def preprocess_data(self):
         self.df['difference'] = self.df['lastPrice'].shift(1) - self.df['lastPrice']
         self.df.dropna(inplace=True)
-        self.df['direction'] = self.df['difference'].apply(lambda x: 1 if x > 0 else -1 if x < 0 else 0)
-        self.X = self.df.iloc[:, 1:-6].values
-        self.y = to_categorical(self.df['direction'].to_numpy(), num_classes=3)
+        self.df['direction'] = self.df['difference'].apply(lambda x: 2 if x > 0 else 0 if x < 0 else 1)
+        self.X = self.df.iloc[:, 1:-3].values
+        self.y = self.df.loc[:, 'direction'].values
 
     def train_test_split(self, test_size=0.3, random_state=42):
         self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(self.X, self.y, test_size=test_size,
-                                                                                random_state=random_state)
+                                                                                random_state=random_state,
+                                                                                shuffle=False)
         self.scaler.fit(self.X_train)
         self.X_train = self.scaler.transform(self.X_train)
         self.X_test = self.scaler.transform(self.X_test)
 
-    def build_model(self):
+    def create_models(self):
+        self.models = {
+            "Dense": self.Dense_model,
+            "CNN": self.Dense_model,
+            "LSTM": self.Dense_model,
+            "RNN": self.Dense_model,
+            "SimpleRNN": self.Dense_model,
+            "CNNRNN": self.Dense_model,
+            "xgboost": self.Dense_model,
+            "treeDecision": self.Dense_model,
+            "LinearRegression": self.Dense_model,
+            "LogisticRegression": self.Dense_model,
+            "KNeighborsClassifier": self.Dense_model,
+            "RandomForestClassifier": self.Dense_model,
+            "GradientBoostingClassifier": self.Dense_model,
+            "SVC": self.Dense_model,
+            "GaussianNB": self.Dense_model,
+            "Perceptron": self.Dense_model,
+            "MLPClassifier": self.Dense_model
+        }
+
+    def Dense_model(self):
+        self.y_train = to_categorical(self.y_train, num_classes=3)
+        self.y_test = to_categorical(self.y_test, num_classes=3)
+        input_layer = Input(shape=(self.X.shape[1],))
+        x = Dense(128, activation='relu')(input_layer)
+        x = Dropout(0.5)(x)
+        x = Dense(64, activation='relu')(x)
+        x = Dropout(0.5)(x)
+        output = Dense(self.y_train.shape[1], activation='softmax')(x)
+        model = Model(inputs=input_layer, outputs=output)
+        model.compile(loss='categorical_crossentropy', optimizer=self.optimizer, metrics=['accuracy'])
+        trainHistory = model.fit(self.X_train, self.y_train, epochs=200000, batch_size=500,
+                                 validation_data=(self.X_test, self.y_test),
+                                 callbacks=[self.early_stop, self.tensorboard_callback, self.lr_schedule])
+        return trainHistory.history['val_accuracy'][-1], trainHistory.history['val_loss'][-1]
+
+    def CNN_model(self):
+        self.y_train = to_categorical(self.y_train, num_classes=3)
+        self.y_test = to_categorical(self.y_test, num_classes=3)
         input_layer = Input(shape=(self.X.shape[1], 1))
         x = Conv1D(filters=32, kernel_size=3, padding='same', activation='relu')(input_layer)
         x = MaxPool1D(pool_size=2)(x)
@@ -46,19 +108,164 @@ class ModelTraining:
         x = Dropout(0.5)(x)
         x = Dense(64, activation='relu')(x)
         x = Dropout(0.5)(x)
-        output = Dense(self.y.shape[1], activation='softmax')(x)
-        self.model = Model(inputs=input_layer, outputs=output)
-        self.model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['acc'])
+        output = Dense(self.y_train.shape[1], activation='softmax')(x)
+        model = Model(inputs=input_layer, outputs=output)
+        model.compile(loss='categorical_crossentropy', optimizer=self.optimizer, metrics=['accuracy'])
+        trainHistory = model.fit(self.X_train, self.y_train, epochs=200000, batch_size=500,
+                                 validation_data=(self.X_test, self.y_test),
+                                 callbacks=[self.early_stop, self.tensorboard_callback, self.lr_schedule])
+        return trainHistory.history['val_accuracy'][-1], trainHistory.history['val_loss'][-1]
 
-    def train_model(self):
-        trainHistory = self.model.fit(self.X_train, self.y_train, epochs=200000, batch_size=500,
-                                      validation_data=(self.X_test, self.y_test),
-                                      callbacks=[self.early_stop, self.tensorboard_callback])
-        return trainHistory
+    def LSTM_model(self):
+        self.y_train = to_categorical(self.y_train, num_classes=3)
+        self.y_test = to_categorical(self.y_test, num_classes=3)
+        input_layer = Input(shape=(self.X.shape[1], 1))
+        x = LSTM(units=64, return_sequences=True)(input_layer)
+        x = Dropout(0.5)(x)
+        x = LSTM(units=32, return_sequences=True)(x)
+        x = Dropout(0.5)(x)
+        x = LSTM(units=16)(x)
+        x = Dropout(0.5)(x)
+        output = Dense(self.y_train.shape[1], activation='softmax')(x)
+        model = Model(inputs=input_layer, outputs=output)
+        model.compile(loss='categorical_crossentropy', optimizer=self.optimizer, metrics=['accuracy'])
+        trainHistory = model.fit(self.X_train, self.y_train, epochs=200000, batch_size=500,
+                                 validation_data=(self.X_test, self.y_test),
+                                 callbacks=[self.early_stop, self.tensorboard_callback, self.lr_schedule])
+        return trainHistory.history['val_accuracy'][-1], trainHistory.history['val_loss'][-1]
+
+    def RNN_model(self):
+        self.y_train = to_categorical(self.y_train, num_classes=3)
+        self.y_test = to_categorical(self.y_test, num_classes=3)
+        input_layer = Input(shape=(self.X.shape[1], 1))
+        x = RNN(units=32, return_sequences=True)(input_layer)
+        x = Dropout(0.5)(x)
+        x = RNN(units=64, return_sequences=False)(x)
+        x = Dropout(0.5)(x)
+        output = Dense(self.y_train.shape[1], activation='softmax')(x)
+        model = Model(inputs=input_layer, outputs=output)
+        model.compile(loss='categorical_crossentropy', optimizer=self.optimizer, metrics=['accuracy'])
+        trainHistory = model.fit(self.X_train, self.y_train, epochs=200000, batch_size=500,
+                                 validation_data=(self.X_test, self.y_test),
+                                 callbacks=[self.early_stop, self.tensorboard_callback, self.lr_schedule])
+        return trainHistory.history['val_accuracy'][-1], trainHistory.history['val_loss'][-1]
+
+    def SimpleRNN_model(self):
+        self.y_train = to_categorical(self.y_train, num_classes=3)
+        self.y_test = to_categorical(self.y_test, num_classes=3)
+        input_layer = Input(shape=(self.X.shape[1], 1))
+        x = SimpleRNN(units=32, return_sequences=True)(input_layer)
+        x = Dropout(0.5)(x)
+        x = SimpleRNN(units=64, return_sequences=False)(x)
+        x = Dropout(0.5)(x)
+        output = Dense(self.y_train.shape[1], activation='softmax')(x)
+        model = Model(inputs=input_layer, outputs=output)
+        model.compile(loss='categorical_crossentropy', optimizer=self.optimizer, metrics=['accuracy'])
+        trainHistory = model.fit(self.X_train, self.y_train, epochs=200000, batch_size=500,
+                                 validation_data=(self.X_test, self.y_test),
+                                 callbacks=[self.early_stop, self.tensorboard_callback, self.lr_schedule])
+        return trainHistory.history['val_accuracy'][-1], trainHistory.history['val_loss'][-1]
+
+    def CNNRNN_model(self):
+        self.y_train = to_categorical(self.y_train, num_classes=3)
+        self.y_test = to_categorical(self.y_test, num_classes=3)
+        input_layer = Input(shape=(self.X.shape[1], 1))
+        x = Conv1D(filters=32, kernel_size=3, padding='same', activation='relu')(input_layer)
+        x = MaxPool1D(pool_size=2)(x)
+        x = Conv1D(filters=64, kernel_size=3, padding='same', activation='relu')(x)
+        x = MaxPool1D(pool_size=2)(x)
+        x = Conv1D(filters=128, kernel_size=3, padding='same', activation='relu')(x)
+        x = Flatten()(x)
+        x = Dense(128, activation='relu')(x)
+        x = Dropout(0.5)(x)
+        x = Dense(64, activation='relu')(x)
+        x = Dropout(0.5)(x)
+        x = Reshape((x.shape[1], 1))(x)
+        x = SimpleRNN(units=64, activation='relu')(x)
+        output = Dense(self.y_train.shape[1], activation='softmax')(x)
+        model = Model(inputs=input_layer, outputs=output)
+        model.compile(loss='categorical_crossentropy', optimizer=self.optimizer, metrics=['accuracy'])
+        trainHistory = model.fit(self.X_train, self.y_train, epochs=200000, batch_size=500,
+                                 validation_data=(self.X_test, self.y_test),
+                                 callbacks=[self.early_stop, self.tensorboard_callback, self.lr_schedule])
+        return trainHistory.history['val_accuracy'][-1], trainHistory.history['val_loss'][-1]
+
+    def xgboost_model(self):
+        self.model = XGBClassifier(use_label_encoder=False)
+        self.model.fit(self.X_train, self.y_train)
+        y_pred = self.model.predict(self.X_test)
+        accuracy = accuracy_score(self.y_test, y_pred)
+        loss = log_loss(self.y_test, y_pred, labels=y_pred)
+        return accuracy, loss
+
+    def treeDecision_model(self):
+        self.model = tree.DecisionTreeClassifier()
+        self.model.fit(self.X_train, self.y_train)
+        y_pred = self.model.predict(self.X_test)
+        accuracy = accuracy_score(self.y_test, y_pred)
+        loss = log_loss(self.y_test, y_pred, labels=y_pred)
+        return accuracy, loss
+
+    def LinearRegression_model(self):
+        self.model = LinearRegression()
+        self.model.fit(self.X_train, self.y_train)
+        y_pred = self.model.predict(self.X_test)
+        accuracy = accuracy_score(self.y_test, y_pred)
+        loss = log_loss(self.y_test, y_pred, labels=y_pred)
+        return accuracy, loss
+
+    def LogisticRegression_model(self):
+        self.model = LogisticRegression()
+        self.model.fit(self.X_train, self.y_train)
+        y_pred = self.model.predict(self.X_test)
+        accuracy = accuracy_score(self.y_test, y_pred)
+        loss = log_loss(self.y_test, y_pred, labels=y_pred)
+        return accuracy, loss
+
+    def KNeighborsClassifier_model(self):
+        self.model = KNeighborsClassifier(n_neighbors=5)
+        self.model.fit(self.X_train, self.y_train)
+        y_pred = self.model.predict(self.X_test)
+        accuracy = accuracy_score(self.y_test, y_pred)
+        loss = log_loss(self.y_test, y_pred, labels=y_pred)
+        return accuracy, loss
+
+    def GradientBoostingClassifier_model(self):
+        self.model = GradientBoostingClassifier(n_estimators=100)
+        self.model.fit(self.X_train, self.y_train)
+        y_pred = self.model.predict(self.X_test)
+        accuracy = accuracy_score(self.y_test, y_pred)
+        loss = log_loss(self.y_test, y_pred, labels=y_pred)
+        return accuracy, loss
+
+    def GaussianNB_model(self):
+        self.model = GaussianNB()
+        self.model.fit(self.X_train, self.y_train)
+        y_pred = self.model.predict(self.X_test)
+        accuracy = accuracy_score(self.y_test, y_pred)
+        loss = log_loss(self.y_test, y_pred, labels=y_pred)
+        return accuracy, loss
+
+    def Perceptron_model(self):
+        self.model = Perceptron()
+        self.model.fit(self.X_train, self.y_train)
+        y_pred = self.model.predict(self.X_test)
+        accuracy = accuracy_score(self.y_test, y_pred)
+        loss = log_loss(self.y_test, y_pred, labels=y_pred)
+        return accuracy, loss
+
+    def MLPClassifier_model(self):
+        self.model = MLPClassifier(hidden_layer_sizes=(100, 100, 100), max_iter=1000)
+        self.model.fit(self.X_train, self.y_train)
+        y_pred = self.model.predict(self.X_test)
+        accuracy = accuracy_score(self.y_test, y_pred)
+        loss = log_loss(self.y_test, y_pred, labels=y_pred)
+        return accuracy, loss
+
 
 if __name__ == '__main__':
-    mt = ModelTraining(database_path=r"C:\Udemy\Interactive Brokers Python API\streaming ES\ES_ticks1.db")
+    mt = ModelTraining(database_path=r".\CL_ticks.db")
     mt.preprocess_data()
     mt.train_test_split()
-    mt.build_model()
-    history = mt.train_model()
+    accuracy, loss = mt.CNNRNN_model()
+    print(accuracy, loss)
--- save_marketdepth_as_sql.py
+++ save_marketdepth_as_sql.py
@@ -1,100 +1,64 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Dec 28 20:07:20 2020
-
-@author: alial
-"""
-
-
-# import libraries
-
 import sqlite3
-
 import numpy as np
 import pandas as pd
-from IPython.display import display, clear_output
 from ib_insync import *
+import random
+import os
 
 
-def onTickerUpdate(ticker):
-    global ticker_size
-    global ticker_price
-    ticker_size.append(ticker.lastSize)
-    ticker_price.append(ticker.last)
-    if len(ticker_size) > 20:
-        ticker_size = [0,0]
-    if len(ticker_price) > 20:
-        ticker_price = [0,0]
-    if ticker_size[-1] == ticker_size[-2] or ticker_price[-1] == ticker_price[-2]:
-        return
-    bids = ticker.domBids
-    for i in range(5):
-        df.iloc[i, 0] = bids[i].size if i < len(bids) else 0
-        df.iloc[i, 1] = bids[i].price if i < len(bids) else 0
-        
-    asks = ticker.domAsks
-    for i in range(5):
-        df.iloc[i, 2] = asks[i].price if i < len(asks) else 0
-        df.iloc[i, 3] = asks[i].size if i < len(asks) else 0
-    last = ticker.domTicks[0].price
-    print(df.columns)
-    x= df.values.flatten()
-    x = np.append(x, last)
-    df_x.index = [ticker.time]
-    print(x.shape, df_x.shape)
-    df_x.iloc[0,0:] = x
-    clear_output(wait=True)
-    display(df)
-    display(last)
-    display(x)
-    display(df_x)
-    df_x.to_sql(name='ES_market_depth', con = db, if_exists='append')
+class StreamData:
+    def __init__(self, db_path="./CL_ticks.db"):
+        self.ticker_size = [0, 0]
+        self.ticker_price = [0, 0]
+        self.df = pd.DataFrame(index=range(5), columns='bidSize bidPrice askPrice askSize'.split())
+        self.df_x = pd.DataFrame(index=range(1), columns='bidSize_1 bidPrice_1 askPrice_1 askSize_1 \
+                                bidSize_2 bidPrice_2 askPrice_2 askSize_2 bidSize_3 \
+                                    bidPrice_3 askPrice_3 askSize_3 bidSize_4 \
+                                        bidPrice_4 askPrice_4 askSize_4 bidSize_5 \
+                                            bidPrice_5 askPrice_5 askSize_5 lastPrice'.split())
+        self.last_price = None
+        self.ib = IB()
+        self.ib.connect('127.0.0.1', 7496, random.randint(0, 100))
+        self.contract = ContFuture(symbol="NQ", exchange="CME", currency="USD")
+        self.ib.qualifyContracts(self.contract)
+        self.ticker = self.ib.reqMktDepth(self.contract)
+        self.db_file = db_path
+        self.db = sqlite3.connect(self.db_file)
+        if not os.path.exists(self.db_file):
+            self.cursor = self.db.cursor()
+            self.cursor.execute('DROP TABLE IF EXISTS ES_market_depth')
+            self.db.commit()
+        else:
+            self.conn = sqlite3.connect(self.db_file)
+            self.cursor = self.conn.cursor()
+
+    def start_stream(self):
+        self.cursor.execute('DROP TABLE IF EXISTS ES_market_depth')
+        self.db.commit()
+        self.ticker.updateEvent += self.on_ticker_update
+        self.ib.run()
+
+    def on_ticker_update(self, ticker):
+        bids = ticker.domBids
+        for i in range(5):
+            self.df.iloc[i, 0] = bids[i].size if i < len(bids) else 0
+            self.df.iloc[i, 1] = bids[i].price if i < len(bids) else 0
+
+        asks = ticker.domAsks
+        for i in range(5):
+            self.df.iloc[i, 2] = asks[i].price if i < len(asks) else 0
+            self.df.iloc[i, 3] = asks[i].size if i < len(asks) else 0
+        self.last_price = ticker.domTicks[0].price
+        x = self.df.values.flatten()
+        x = np.append(x, self.last_price)
+        self.df_x.iloc[0, :] = x
+        self.df_x.to_sql(name='ES_market_depth', con=self.db, if_exists='append')
 
 
-
-util.startLoop() #required for ib_insync to use in kernals of jupyter and spyder
-ib = IB() #initialize IB class for IB API Client and Wrapper async version
-ib.disconnect() #to start a new connection disconnect older ones
-ib.connect('127.0.0.1',7496,8) #connect to IB server
-ib.portfolio()
-
-ticker_size = [0,0] #initialize arrays for size 
-ticker_price = [0,0] #initialize arrays for price 
+def main():
+    stream = StreamData('./CL_ticks.db')
+    stream.start_stream()
 
 
-contract=Future(conId=256019251, symbol='CL', lastTradeDateOrContractMonth='20211220', right='0', multiplier='1000', primaryExchange='NYMEX', currency='USD', localSymbol='CLF2', tradingClass='CL') #define contract
-
-ib.qualifyContracts(contract) #qualify contract
-
-ticker = ib.reqMktDepth(contract) #request market depth for the ticker
-df = pd.DataFrame(index=range(5),
-        columns='bidSize bidPrice askPrice askSize'.split()) #save the ticker in a df
-
-df_x = pd.DataFrame(index = range(1), 
-                    columns= 'bidSize_1 bidPrice_1 askPrice_1 askSize_1 \
-                        bidSize_2 bidPrice_2 askPrice_2 askSize_2 bidSize_3 \
-                            bidPrice_3 askPrice_3 askSize_3 bidSize_4 \
-                                bidPrice_4 askPrice_4 askSize_4 bidSize_5 \
-                                    bidPrice_5 askPrice_5 askSize_5 lastPrice'\
-                                        .split()) #updating db raws 
-
-    
-""" initialize sql file """
-
-db = sqlite3.connect(r'C:\Udemy\Interactive Brokers Python API\streaming ES\CL_ticks.db')
-c=db.cursor()
-c.execute('DROP TABLE IF EXISTS ES_market_depth')
-# c.execute("CREATE TABLE ES_market_depth (time datetime primary key,bidSize_1 integer, bidPrice_1 real(15,5), askPrice_1 real(15,5), askSize_1 integer, bidSize_2 integer, bidPrice_2 real(15,5), askPrice_2 real(15,5), askSize_2 integer, bidSize_3 integer, bidPrice_3 real(15,5), askPrice_3 real(15,5), askSize_3 integer, bidSize_4 integer, bidPrice_4 real(15,5), askPrice_4 real(15,5), askSize_4 integer, bidSize_5 integer, bidPrice_5 real(15,5), askPrice_5 real(15,5), askSize_5 integer, lastPrice real(15,5))")
-try:
-    db.commit()
-except:
-    db.rollback()
-    
-# ib.disconnect()
-    
-    
-    
-ib.sleep(2)
-ticker.updateEvent += onTickerUpdate
-
-IB.sleep(15);
+if __name__ == "__main__":
+    main()
